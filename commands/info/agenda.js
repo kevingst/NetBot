2fd2d073ea6f535d01a4ce23ee24183e870beace
Kriiox <36078238+kriiox@users.noreply.github.com>
2020-04-05 14:02:11 +0200

    Update agenda.js

--- commands/info/agenda.js
+++ commands/info/agenda.js
@@ -79,12 +79,6 @@ module.exports = {
     }
 }
 
-// function getNumberOfWeek(date) {
-//     const firstDayOfYear = new Date(date.getFullYear(), 0, 1);
-//     const pastDaysOfYear = (date - firstDayOfYear) / 86400000;
-//     return Math.ceil((pastDaysOfYear + firstDayOfYear.getDay() + 1) / 7);
-// }
-
 function getNumberOfWeek(dt) {
     var tdt = new Date(dt.valueOf());
     var dayn = (dt.getDay() + 6) % 7;
